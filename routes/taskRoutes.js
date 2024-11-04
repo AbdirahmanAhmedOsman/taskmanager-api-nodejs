c9adb20dc66cbdddb1d5a7f94ff2ee753e41fa3f
Abdirahman Ahmed Osman <fareyofficial11@gmail.com>
2024-11-04 08:32:45 +0300

    change

---
+++ routes/taskRoutes.js
@@ -0,0 +1,16 @@
+const taskRoutes= (req, res)=>{
+    if(req.method === 'GET'){
+        getTask(req,req);
+    }else if(req.method === 'PST'){
+        createTask(req,res);
+    }else if(req.method === 'PATCH'){
+        updateTask(req,e=res);
+    }else if(req.method === 'DELETE'){
+        deleteTask(req,res);
+    }else{
+        res.writeHeade(404, 'Data Not Found', {'content-type': 'application/json'})
+        res.end(JSON.stringify({
+            message :" Unknown Method Required"
+        }))
+    }
+}
