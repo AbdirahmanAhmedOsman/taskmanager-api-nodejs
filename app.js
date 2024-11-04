5be8c64ea050a26cb5507127835034df8e9902bc
Abdirahman Ahmed Osman <fareyofficial11@gmail.com>
2024-11-04 08:20:24 +0300

    change

--- app.js
+++ app.js
@@ -3,4 +3,18 @@ const http = require('http');
 const hostname= 'localost';
 const port = 9000
 
-const server = http.createServer(req, res)
+const server = http.createServer((req,res) =>{
+    if(req.url.startsWith('/tasks')){
+        taskRoutes(req,res)
+    }else{
+        res.writeHead(404,'No tFound',{'content-type': 'application/json'})
+        res.end(JSON.stringify({
+            message:"Sorry you got lost"
+        }))
+    }
+
+});
+
+server.listen(port,hostname, ()=>{
+    console.log(`Server running on port ${post}`)
+})
