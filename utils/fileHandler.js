532cb91c6a238bca246cad9e6e7d47cb74beb22a
Abdirahman Ahmed Osman <fareyofficial11@gmail.com>
2024-11-05 05:25:32 +0300

    change

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,3 +1,16 @@
 const fs= require('fs');
 const path= require('path');
 const filePath = './data/tasks.json';
+
+const writeTasksToFile =(tasks)=>{
+    fs.writeFileSync(filePath, JSON.stringify(tasks))
+}
+
+const readTasksFromFile=()=>{
+    if (!fs.existsSync(filePath)){
+        writeTasksToFile([])
+    }
+
+    const data = fs.readFileSync(filePath);
+    return JSON.parse(data)
+}
